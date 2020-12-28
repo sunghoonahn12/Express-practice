9aa67d01e5a0a51120a3b874ad66de9b34026e47
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2020-12-28 04:38:55 -0500

    served image in public folder

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser')
 var compression = require('compression')
 var port = 3000
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 // only for get request
@@ -24,7 +25,10 @@ app.get('/', (request, response) => {
   var description = 'Hello, Node.js';
   var list = template.list(request.list);
   var html = template.HTML(title, list,
-    `<h2>${title}</h2>${description}`,
+    `
+    <h2>${title}</h2>${description}
+    <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
+    `,
     `<a href="/create">create</a>`
   );
   response.send(html);
