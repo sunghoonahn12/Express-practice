d132d4d52ed41cec6de4b1d668e65c4b0a659eaf
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2020-12-28 13:39:38 -0500

    changed the router path

--- main.js
+++ main.js
@@ -29,12 +29,86 @@ app.get('/', (request, response) => {
     <h2>${title}</h2>${description}
     <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
     `,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   response.send(html);
 })
 
-app.get('/page/:pageId', (request, response, next) => {
+app.get('/topic/create', (request, response) => {
+  var title = 'WEB - create';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+    <form action="/topic/create_process" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+        <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+  `, '');
+  response.send(html);
+})
+
+app.post('/topic/create_process', (request, response) => {
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    // redirection to /page/${title}
+    response.writeHead(302, {Location: `/topic/${title}`});
+    response.end();
+ });
+})
+
+app.get('/topic/update/:pageId', (request, response) => {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `
+      <form action="/topic/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `,
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+    );
+    response.send(html)
+  });
+})
+
+app.post('/topic/update_process', (request, response) => {
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/topic/${title}`)
+      response.end();
+    })
+  });
+})
+
+app.post('/topic/delete_process', (request, response) => {
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error){
+    response.redirect('/')
+  });
+});
+
+app.get('/topic/:pageId', (request, response, next) => {
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
     if(err){
@@ -48,9 +122,9 @@ app.get('/page/:pageId', (request, response, next) => {
       var list = template.list(request.list);
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
+        ` <a href="/topic/create">create</a>
+          <a href="/topic/update/${sanitizedTitle}">update</a>
+          <form action="/topic/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
@@ -60,79 +134,6 @@ app.get('/page/:pageId', (request, response, next) => {
   });
 })
 
-app.get('/create', (request, response) => {
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-})
-
-app.post('/create_process', (request, response) => {
-   var post = request.body;
-   var title = post.title;
-   var description = post.description;
-   fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-     // redirection to /page/${title}
-     response.writeHead(302, {Location: `/page/${title}`});
-     response.end();
-  });
-})
-
-app.get('/update/:pageId', (request, response) => {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-      <form action="/update_process" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-    );
-    response.send(html)
-  });
-})
-
-app.post('/update_process', (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function(error){
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/page/${title}`)
-      response.end();
-    })
-  });
-})
-
-app.post('/delete_process', (request, response) => {
-    var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error){
-      response.redirect('/')
-    });
-});
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that')
