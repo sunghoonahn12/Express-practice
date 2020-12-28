cbc1850cabccf94e874fd83c46ec9af4f5847efb
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2020-12-28 03:13:21 -0500

    made routing for updating and deleting page

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ app.get('/page/:pageId', (request, response) => {
       var html = template.HTML(sanitizedTitle, list,
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
-          <a href="/update?id=${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
+          <a href="/update/${sanitizedTitle}">update</a>
+          <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
@@ -73,8 +73,69 @@ app.post('/create_process', (request, response) => {
           var title = post.title;
           var description = post.description;
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            // redirection to /page/${title}
             response.writeHead(302, {Location: `/page/${title}`});
             response.end();
+          })
+      });
+})
+
+app.get('/update/:pageId', (request, response) => {
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var list = template.list(filelist);
+      var html = template.HTML(title, list,
+        `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+      );
+      response.send(html)
+    });
+  });
+})
+
+app.post('/update_process', (request, response) => {
+  var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.redirect(`/page/${title}`)
+              response.end();
+            })
+          });
+      });
+})
+
+app.post('/delete_process', (request, response) => {
+  var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var filteredId = path.parse(id).base;
+          fs.unlink(`data/${filteredId}`, function(error){
+            response.redirect('/')
           })
       });
 })
