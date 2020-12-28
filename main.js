15204323b6f76390096c559693ed13d1807782e1
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2020-12-27 23:07:12 -0500

    created home page

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const template = require('./lib/template.js')
+
+
+const port = 3000
+
+app.get('/', (request, response) => {
+  fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    response.send(html);
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+/*
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -141,3 +167,4 @@ var app = http.createServer(function(request,response){
     }
 });
 app.listen(3000);
+*/
