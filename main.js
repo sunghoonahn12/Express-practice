092322c5d2e552480d29d16b7103fdac2fef35f7
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2020-12-27 23:58:12 -0500

    used routing to create list

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-const template = require('./lib/template.js')
+const template = require('./lib/template.js');
 const path = require('path');
 const sanitizeHtml = require('sanitize-html');
+const qs = require('querystring');
 const port = 3000
 
 app.get('/', (request, response) => {
@@ -41,6 +42,41 @@ app.get('/page/:pageId', (request, response) => {
       response.send(html);
     });
   });
+})
+
+app.get('/create', (request, response) => {
+  fs.readdir('./data', function(error, filelist){
+    var title = 'WEB - create';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list, `
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, '');
+    response.send(html);
+  });
+})
+
+app.post('/create_process', (request, response) => {
+  var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/page/${title}`});
+            response.end();
+          })
+      });
 })
 
 app.listen(port, () => {
